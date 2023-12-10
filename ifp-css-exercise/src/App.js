00cb045164a112ce3ede62d41e0ecc834e4a1cdc
Fabriccio Tornero <fabriccio.tornerocortes@extendhealth.com>
2023-12-10 16:38:57 -0500

    Finish the task

--- ifp-css-exercise/src/App.js
+++ ifp-css-exercise/src/App.js
@@ -1,23 +1,69 @@
 import './App.css';
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import viaTheme from "es-components-via-theme";
 import BackButton from './components/BackButton';
+import PlanHeader from './components/PlanHeader';
+import plans from './plan-data.json';
+
+const PlanHeaderNav = styled.section`
+  display: grid;
+  height: auto;
+
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  grid-template-areas: 
+    "plan1 plan2";
+
+  /* Targeting the PlanHeader components */
+  > section:nth-child(1) {
+    grid-area: plan1;
+    border-right: 1px solid gray;
+  }
+  > section:nth-child(2) {
+    grid-area: plan2;
+  }
+  
+  > section:nth-child(3) {
+    visibility: collapse;
+  }
+
+    @media (min-width: ${props => props.theme.screenSize.tablet}) {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+
+      grid-template-areas: 
+      ". plan1 plan2 plan3";
+
+      > section:nth-child(1) {
+        border-left: 1px solid gray;
+      }
+      > section:nth-child(2) {
+        border-right: 1px solid gray;
+      }
+      > section:nth-child(3) {
+        grid-area: plan3;
+        visibility: visible;
+      }
+    }
+`;
 
 function App() {
+
   return (
+    <div style={{padding:10}}>
     <ThemeProvider theme={viaTheme}>
         <header className="App-header">
           <BackButton></BackButton>
         </header>
         <body>
-          <div className='task-2'>
-          <div></div>
-          <div>Jose</div>
-          <div>Jesus</div>
-          <div>Maria</div>
-          </div>
+          <PlanHeaderNav>
+          {plans.map((plan, index) => (
+                <PlanHeader key={index} PlanName={plan.planName} Premium={plan.premium} logoUrl={plan.logo}>
+                </PlanHeader>
+            ))}
+          </PlanHeaderNav>
         </body>
     </ThemeProvider>
+    </div>
   );
 }
 
